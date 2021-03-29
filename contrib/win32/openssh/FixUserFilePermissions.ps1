9e37d25524faf17a6c8da24c5b88cff729b05606
Vivian Thiebaut <vthiebaut@microsoft.com>
2021-03-29 15:39:52 -0400

    FixUserFilePermissions automatically fixes permissions for administrators_authorized_keys

--- contrib/win32/openssh/FixUserFilePermissions.ps1
+++ contrib/win32/openssh/FixUserFilePermissions.ps1
@@ -14,5 +14,15 @@ Get-ChildItem ~\.ssh\* -Include "id_rsa","id_dsa" -ErrorAction SilentlyContinue 
     Repair-UserKeyPermission -FilePath $_.FullName @psBoundParameters
 }
 
+$sshdAdministratorsAuthorizedKeysPath = join-path $env:ProgramData\ssh "administrators_authorized_keys"
+if(Test-Path $sshdAdministratorsAuthorizedKeysPath -PathType Leaf) 
+{
+    Repair-AdministratorsAuthorizedKeysPermission -FilePath $sshdAdministratorsAuthorizedKeysPath @psBoundParameters
+}
+else 
+{
+    Write-host "$sshdConfigPath does not exist"  -ForegroundColor Yellow
+}
+
 Write-Host "   Done."
 Write-Host " "
