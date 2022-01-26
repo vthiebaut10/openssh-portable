fa620ac7df4703df5f47747d7172ed1e6992dff6
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-01-26 10:08:22 -0500

    Run only transfer.sh from the bash tests

--- contrib/win32/openssh/AppveyorHelper.psm1
+++ contrib/win32/openssh/AppveyorHelper.psm1
@@ -359,7 +359,6 @@ function Invoke-OpenSSHTests
     }
 
     # Run UNIX bash tests.
-    '''
     Invoke-OpenSSHBashTests
     if (-not $Global:bash_tests_summary)
     {
@@ -382,7 +381,6 @@ function Invoke-OpenSSHTests
         Write-Warning "Stop running further tests!"
         return
     }
-    '''
 
     Invoke-OpenSSHUninstallTest
     if (($OpenSSHTestInfo -eq $null) -or (-not (Test-Path $OpenSSHTestInfo["UninstallTestResultsFile"])))
