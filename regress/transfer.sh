12d32938f0fa6c1f44f8435e88f0f54d483e9e03
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-01-28 13:26:49 -0500

    trigger tests

--- regress/transfer.sh
+++ regress/transfer.sh
@@ -11,14 +11,14 @@ if [ $? -ne 0 ]; then
 fi
 cmp ${DATA} ${COPY}		|| fail "corrupted copy"
 
-#for s in 10 100 1k 32k 64k 128k 256k; do
-#	trace "dd-size ${s}"
-#	rm -f ${COPY}
-#	dd if=$DATA obs=${s} 2> /dev/null | \
-#		${SSH} -q -F $OBJ/ssh_proxy somehost "cat > ${COPY}"
-#	if [ $? -ne 0 ]; then
-#		fail "ssh cat $DATA failed"
-#	fi
-#	cmp $DATA ${COPY}		|| fail "corrupted copy"
-#done
+for s in 10 100 1k 32k 64k 128k 256k; do
+	trace "dd-size ${s}"
+	rm -f ${COPY}
+	dd if=$DATA obs=${s} 2> /dev/null | \
+		${SSH} -q -F $OBJ/ssh_proxy somehost "cat > ${COPY}"
+	if [ $? -ne 0 ]; then
+		fail "ssh cat $DATA failed"
+	fi
+	cmp $DATA ${COPY}		|| fail "corrupted copy"
+done
 rm -f ${COPY}
