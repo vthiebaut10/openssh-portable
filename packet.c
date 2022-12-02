a795ef64f3a0b06d1db23f94b0058a033c76573f
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-12-02 12:37:08 -0500

    Fix diplicated return statement

--- packet.c
+++ packet.c
@@ -1449,8 +1449,7 @@ ssh_packet_read_poll2_mux(struct ssh *ssh, u_char *typep, u_int32_t *seqnr_p)
 		return SSH_ERR_INTERNAL_ERROR;
 	*typep = SSH_MSG_NONE;
 	cp = sshbuf_ptr(state->input);
-	if (cp == NULL) // fix CodeQL SM02311
-		return SSH_ERR_INTERNAL_ERROR;
+	if (cp == NULL) { // fix CodeQL SM02311
 		return SSH_ERR_INTERNAL_ERROR;
 	}
 	if (state->packlen == 0) {
